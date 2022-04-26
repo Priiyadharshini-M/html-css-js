231b792c480cff05d5d88eee20f40e14f6285df2
Priiyadharshini-M <96585529+Priiyadharshini-M@users.noreply.github.com>
2022-04-26 11:17:56 +0530

    Update exception.js

--- exception.js
+++ exception.js
@@ -16,3 +16,38 @@ files.forEach(file=>
             }
         }
     });
+
+
+fs.readFile(files[1],'utf-8',(err,data)=>{
+    if(err) throw err;
+    console.log(data);
+});
+
+//using process   
+process.on('uncaughtException',err=>console.log('The exception was caught... using process')); 
+
+//using domain
+let d=require('domain').create()
+d.run(()=>{
+    fs.readFile("./dem.txt",'utf-8',(err,data)=>{
+        if(err) throw err
+        console.log(data);
+    });
+});
+
+d.on("error",(err)=>console.log("The Error was caught... using domain"))
+
+//using promises
+const promise = new Promise(function (resolve, reject) {
+    reject(new Error('Something was wrong...'));
+});
+
+promise.then(() => {
+        console.log("hi");
+    })
+    .then(() => {
+        console.log('hello'); 
+    })
+    .catch(err => {
+        console.log(err.message); 
+    })
